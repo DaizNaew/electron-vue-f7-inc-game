bf2a6853ef1e7a95846a71023b3af44d334a486b
DaizNaew <daiznaew@gmail.com>
2019-05-27 14:40:10 +0200

    Added crude data and user saving

--- src/mixins/gameData.js
+++ src/mixins/gameData.js
@@ -1,16 +1,35 @@
+const Store = require('../scripts/store');
+
+const store = new Store({
+    configName: 'user-data',
+    defaults: {
+        gameData: {
+            Clicks: 0,
+            TotalClicks: 0,
+            clicksPerClicks: 1,
+            ClicksPerSecond: 0,
+            CostOfNextClickUpgrade: 10,
+            ClicksUpgraded: 0,
+            Tickrate: 10,
+            BuyAmount: 1,
+        }
+    }
+})
+let {Clicks, TotalClicks, clicksPerClicks, ClicksPerSecond, CostOfNextClickUpgrade, ClicksUpgraded, Tickrate, BuyAmount} = store.get('gameData');
 export const gameData = {
+    
     data() {
       return {
           NameOfObjectToGet: 'UwU',
-          Clicks: 0,
-          TotalClicks: 0,
-          clicksPerClicks: 1,
-          ClicksPerSecond: 0,
-          CostOfNextClickUpgrade: 10,
           ClickUpgradeCostMod: 1.07,
-          ClicksUpgraded: 0,
-          Tickrate: 10,
-          BuyAmount: 1,
+          Clicks,
+          TotalClicks,
+          clicksPerClicks,
+          ClicksPerSecond,
+          CostOfNextClickUpgrade,
+          ClicksUpgraded,
+          Tickrate,
+          BuyAmount,
           Buildings: [
             {id: 0, name: 'Animu newb', cost: 10, base_prod: 0.1, amount_owned: 0, total_prod: 0,
                 Upgrades: 
@@ -87,9 +106,51 @@ export const gameData = {
             return Math.ceil(building.cost * Math.pow(this.ClickUpgradeCostMod, building.amount_owned));
         },
         ticker: function() {
+            let tick = 0;
+            const self = this;
+            const app = self.$f7;
+            const $$ = self.$$;
             setInterval(() => {
-                this.tickClick(this.ClicksPerSecond*this.Tickrate);
-            },this.Tickrate*1000)
+                tick += 1000;
+                if(tick >= 1000*this.Tickrate) {
+                    let profit = this.ClicksPerSecond*this.Tickrate;
+                    this.tickClick(profit);
+                    let tickText = `<p id="tickText" style="opacity:0;position:absolute;z-index:100;left:58%; bottom:60%">+${profit.toFixed(1)}</p>`;
+                    $$('#floatingTickText').append(tickText)
+                    $$('#tickText').animate({
+                        'opacity': 1,
+                        'top': '95%'
+                    },
+                    {
+                        duration: 2500,
+                        begin: function(elements) {
+                            self.setTickerProgress(0);
+                        }
+                    })
+                    .animate({
+                        'opacity': 0,
+                        'top': '0px'
+                    },
+                    {
+                        duration: 500,
+                        complete: function(elements) {
+                            $$('#floatingTickText').empty();
+                        }
+                    });
+                    tick = 0;
+                }
+                this.setTickerProgress(tick/100);
+            },1000);
+            setInterval(() => {
+                app.toast.show({
+                    destroyOnClose: true,
+                    text: 'Game Data Saved',
+                    closeTimeout: 2500,
+                    closeButton: true,
+                    closeButtonText: 'X',
+                });
+                self.saveGameData();
+            },1000*60*1)
         },
         allBuildingTotalProduction: function() {
             let prod = 0;
@@ -97,6 +158,28 @@ export const gameData = {
                 prod+=building.total_prod
             })
             this.ClicksPerSecond=prod;
+        },
+        saveGameData:function() {
+            self = this;
+            let {
+                Clicks, TotalClicks, clicksPerClicks, ClicksPerSecond, CostOfNextClickUpgrade, ClicksUpgraded, Tickrate, BuyAmount
+                } = self.returnUserGameData();
+            store.set('gameData', 
+                {Clicks, TotalClicks, clicksPerClicks, ClicksPerSecond, CostOfNextClickUpgrade, ClicksUpgraded, Tickrate, BuyAmount}
+            )
+        },
+        returnUserGameData:function() {
+            let data = {
+                Clicks : self.Clicks, 
+                TotalClicks : self.TotalClicks, 
+                clicksPerClicks : self.clicksPerClicks, 
+                ClicksPerSecond : self.ClicksPerSecond, 
+                CostOfNextClickUpgrade : self.CostOfNextClickUpgrade, 
+                ClicksUpgraded : self.ClicksUpgraded, 
+                Tickrate : self.Tickrate, 
+                BuyAmount : self.BuyAmount
+            }
+            return data
         }
     },
 }
