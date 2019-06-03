4406b5e3982d5c0be218259864695a6a97c41c7c
Daiz Naew <daiznaew@gmail.com>
2019-06-03 15:30:30 +0200

    Made a start on the dark mode theme

--- src/mixins/gameData.js
+++ src/mixins/gameData.js
@@ -15,104 +15,137 @@ const store = new Store({
         }
     }
 })
-let {Clicks, TotalClicks, clicksPerClicks, ClicksPerSecond, CostOfNextClickUpgrade, ClicksUpgraded, Tickrate, BuyAmount} = store.get('gameData');
+let {
+    Clicks,
+    TotalClicks,
+    clicksPerClicks,
+    ClicksPerSecond,
+    CostOfNextClickUpgrade,
+    ClicksUpgraded,
+    Tickrate,
+    BuyAmount
+} = store.get('gameData');
 export const gameData = {
-    
+
     data() {
-      return {
-          NameOfObjectToGet: 'UwU',
-          ClickUpgradeCostMod: 1.07,
-          Clicks,
-          TotalClicks,
-          clicksPerClicks,
-          ClicksPerSecond,
-          CostOfNextClickUpgrade,
-          ClicksUpgraded,
-          Tickrate,
-          BuyAmount,
-          BoughtBuildings: [
-          ],
-          Buildings: [
-            {id: 0, name: 'Animu newb', cost: 10, base_prod: 0.1, amount_owned: 0, total_prod: 0},
-            {id: 1, name: 'Weabu', cost: 100, base_prod: 1, amount_owned: 0, total_prod: 0},
-            {id: 2, name: 'Otaku', cost: 2500, base_prod: 5, amount_owned: 0, total_prod: 0},
-            {id: 3, name: 'Konrad', cost: 10000, base_prod: 10, amount_owned: 0, total_prod: 0},
-          ],
-          
-      }
+        return {
+            NameOfObjectToGet: 'UwU',
+            ClickUpgradeCostMod: 1.07,
+            Clicks,
+            TotalClicks,
+            clicksPerClicks,
+            ClicksPerSecond,
+            CostOfNextClickUpgrade,
+            ClicksUpgraded,
+            Tickrate,
+            BuyAmount,
+            BoughtBuildings: [],
+            Buildings: [{
+                    id: 0,
+                    name: 'Animu newb',
+                    cost: 10,
+                    base_prod: 0.1,
+                    amount_owned: 0,
+                    total_prod: 0
+                },
+                {
+                    id: 1,
+                    name: 'Weabu',
+                    cost: 100,
+                    base_prod: 1,
+                    amount_owned: 0,
+                    total_prod: 0
+                },
+                {
+                    id: 2,
+                    name: 'Otaku',
+                    cost: 2500,
+                    base_prod: 5,
+                    amount_owned: 0,
+                    total_prod: 0
+                },
+                {
+                    id: 3,
+                    name: 'Konrad',
+                    cost: 10000,
+                    base_prod: 10,
+                    amount_owned: 0,
+                    total_prod: 0
+                },
+            ],
+
+        }
     },
-    
+
     created() {
         console.log('Mixin loaded');
     },
     methods: {
         incClick: function () {
-            this.Clicks+=this.clicksPerClicks;
-            this.TotalClicks+=this.clicksPerClicks;
+            this.Clicks += this.clicksPerClicks;
+            this.TotalClicks += this.clicksPerClicks;
         },
         decClick: function () {
             this.Clicks--;
         },
-        tickClick: function(tick) {
+        tickClick: function (tick) {
             this.Clicks += tick;
         },
-        increaseClicksPerClick: function(mod) {
+        increaseClicksPerClick: function (mod) {
             this.clicksPerClicks += mod;
-            this.Clicks-=this.CostOfNextClickUpgrade;
+            this.Clicks -= this.CostOfNextClickUpgrade;
             this.ClicksUpgraded++;
             this.CostOfNextClickUpgrade = Math.ceil(this.CostOfNextClickUpgrade * Math.pow(this.ClickUpgradeCostMod, this.ClicksUpgraded));
         },
-        buyBuilding: function(buildingID) {
+        buyBuilding: function (buildingID) {
 
-            let building = this.Buildings.find( building => building.id === buildingID);
-            this.Clicks-=building.cost;
+            let building = this.Buildings.find(building => building.id === buildingID);
+            this.Clicks -= building.cost;
 
             building.amount_owned++;
             let cost = this.costBuildingCalculator(building)
             building.cost = cost;
-            building.total_prod=(building.amount_owned*building.base_prod);
+            building.total_prod = (building.amount_owned * building.base_prod);
 
             this.allBuildingTotalProduction();
         },
-        costBuildingCalculator: function(building) {
+        costBuildingCalculator: function (building) {
             return Math.ceil(building.cost * Math.pow(this.ClickUpgradeCostMod, building.amount_owned));
         },
-        ticker: function() {
+        ticker: function () {
             let tick = 0;
             const self = this;
             const app = self.$f7;
             const $$ = self.$$;
             setInterval(() => {
                 tick += 1000;
-                if(tick >= 1000*this.Tickrate) {
-                    let profit = this.ClicksPerSecond*this.Tickrate;
+                if (tick >= 1000 * this.Tickrate) {
+                    let profit = this.ClicksPerSecond * this.Tickrate;
                     this.tickClick(profit);
                     let tickText = `<p id="tickText" style="opacity:0;position:absolute;z-index:100;left:58%; bottom:60%">+${profit.toFixed(1)}</p>`;
                     $$('#floatingTickText').append(tickText)
                     $$('#tickText').animate({
-                        'opacity': 1,
-                        'top': '95%'
-                    },
-                    {
-                        duration: 2500,
-                        begin: function(elements) {
-                            self.setTickerProgress(0);
-                        }
-                    })
-                    .animate({
-                        'opacity': 0,
-                        'top': '0px'
-                    },
-                    {
-                        duration: 500,
-                        complete: function(elements) {
-                            $$('#floatingTickText').empty();
-                        }
-                    });
+                            'opacity': 1,
+                            'top': '95%'
+                        }, {
+                            duration: 2500,
+                            begin: function (elements) {
+                                self.setTickerProgress(0);
+                            }
+                        })
+                        .animate({
+                            'opacity': 0,
+                            'top': '0px'
+                        }, {
+                            duration: 500,
+                            complete: function (elements) {
+                                $$('#floatingTickText').empty();
+                            }
+                        });
                     tick = 0;
                 }
-                this.setTickerProgress(tick/100);
-            },1000);
+                this.setTickerProgress(tick / 100);
+            }, 1000);
             setInterval(() => {
                 app.toast.show({
                     destroyOnClose: true,
@@ -122,34 +155,48 @@ export const gameData = {
                     closeButtonText: 'X',
                 });
                 self.saveGameData();
-            },1000*60*1)
+            }, 1000 * 60 * 1)
         },
-        allBuildingTotalProduction: function() {
+        allBuildingTotalProduction: function () {
             let prod = 0;
-            this.Buildings.forEach( building => {
-                prod+=building.total_prod
+            this.Buildings.forEach(building => {
+                prod += building.total_prod
             })
-            this.ClicksPerSecond=prod;
+            this.ClicksPerSecond = prod;
         },
-        saveGameData:function() {
+        saveGameData: function () {
             self = this;
             let {
-                Clicks, TotalClicks, clicksPerClicks, ClicksPerSecond, CostOfNextClickUpgrade, ClicksUpgraded, Tickrate, BuyAmount
-                } = self.returnUserGameData();
-            store.set('gameData', 
-                {Clicks, TotalClicks, clicksPerClicks, ClicksPerSecond, CostOfNextClickUpgrade, ClicksUpgraded, Tickrate, BuyAmount}
-            )
+                Clicks,
+                TotalClicks,
+                clicksPerClicks,
+                ClicksPerSecond,
+                CostOfNextClickUpgrade,
+                ClicksUpgraded,
+                Tickrate,
+                BuyAmount
+            } = self.returnUserGameData();
+            store.set('gameData', {
+                Clicks,
+                TotalClicks,
+                clicksPerClicks,
+                ClicksPerSecond,
+                CostOfNextClickUpgrade,
+                ClicksUpgraded,
+                Tickrate,
+                BuyAmount
+            })
         },
-        returnUserGameData:function() {
+        returnUserGameData: function () {
             let data = {
-                Clicks : self.Clicks, 
-                TotalClicks : self.TotalClicks, 
-                clicksPerClicks : self.clicksPerClicks, 
-                ClicksPerSecond : self.ClicksPerSecond, 
-                CostOfNextClickUpgrade : self.CostOfNextClickUpgrade, 
-                ClicksUpgraded : self.ClicksUpgraded, 
-                Tickrate : self.Tickrate, 
-                BuyAmount : self.BuyAmount
+                Clicks: self.Clicks,
+                TotalClicks: self.TotalClicks,
+                clicksPerClicks: self.clicksPerClicks,
+                ClicksPerSecond: self.ClicksPerSecond,
+                CostOfNextClickUpgrade: self.CostOfNextClickUpgrade,
+                ClicksUpgraded: self.ClicksUpgraded,
+                Tickrate: self.Tickrate,
+                BuyAmount: self.BuyAmount
             }
             return data
         }
--- src/pages/panel-left.vue
+++ src/pages/panel-left.vue
@@ -14,8 +14,37 @@
       <f7-list-item link="/about/" title="About" view="#main-view" panel-close></f7-list-item>
       <f7-list-item link="/form/" title="Form" view="#main-view" panel-close></f7-list-item>
     </f7-list>
+    
+    <f7-list>
+      <f7-list-item>
+        <span>Dark Mode Toggle</span>
+        <f7-toggle id="darkMode" @toggle:change="init()"></f7-toggle>
+      </f7-list-item>
+    </f7-list>
   </f7-page>
 </template>
 <script>
-export default {}
+export default {
+
+  data() {
+    return{
+      darkMode: false,
+    }
+  },
+  
+  methods: {
+    init() {
+      const self = this;
+      const app = self.$f7;
+      var toggle = app.toggle.get('#darkMode');
+      if(toggle.checked) {
+        this.darkMode = true;
+        self.$$('body').toggleClass('theme-dark');
+      } else {
+        this.darkMode = false;
+        self.$$('body').toggleClass('theme-dark');
+      }
+    }
+  }
+}
 </script>
